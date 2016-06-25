7396ac35b394a94093625438686d19b61b6393c0
Aleksandr Petrov <AKPetrov1.SBT@sberbank.ru>
2016-06-25 18:53:06 +0300

    Подключен socket.io для взаимодействия back<-->front

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-
-
-
 var express = require('express');
-var app = express(),
-    path = require('path');
+var app = express();
+var path = require('path');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, './app')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './app/index.html'));
+server.listen(3000, function () {
+  console.log('App listening on port 3000');
 });
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000');
+io.on('connection', function (socket) {
+    console.log('User connected. Socket id %s', socket.id);
+
+    socket.on('disconnect', function () {
+        console.log('User disconnected. %s. Socket id %s', socket.id);
+    });
 });
